396ed783d3c542c3afaa79ca99d8c7364875ae4d
Efosa Osazuwa <efosazuwa0@gmail.com>
2019-04-27 21:04:13 -0600

    added some settings to the typing javascript so it looks better

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".typed", {
-        strings: ["Data Scientist", "Data Viz", "Machine Learning"]
+        strings: ["Data Science", "Data Viz", "Machine Learning"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
     })
 });
