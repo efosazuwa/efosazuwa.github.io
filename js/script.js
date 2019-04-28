daeec969e01a4f0ebb8501d54c8cc43fbb996b54
Efosa Osazuwa <efosazuwa0@gmail.com>
2019-04-27 20:10:04 -0600

    implemented typed javascript effect on sub title message

--- js/script.js
+++ js/script.js
@@ -5,4 +5,8 @@ $(document).ready(function() {
         play: "2500",
         pagination: false
     });
+
+    var typed = new Typed(".typed", {
+        strings: ["Data Scientist", "Data Viz", "Machine Learning"]
+    })
 });
