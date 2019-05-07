746b23a0d22d7d1657404ba70a021f87ee593250
Efosa Osazuwa <efosazuwa0@gmail.com>
2019-05-07 09:30:31 -0600

    add code to make isotope to filter items on click

--- js/script.js
+++ js/script.js
@@ -82,5 +82,25 @@ $(document).ready(function() {
         }
     });
 
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+    });
+
+
 
 });
