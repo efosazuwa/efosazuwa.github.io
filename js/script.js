fad80a03833a9a80bf4143e604a8564c66e52624
Efosa Osazuwa <efosazuwa0@gmail.com>
2019-05-06 19:44:08 -0600

    Added options for isotope's behavior

--- js/script.js
+++ js/script.js
@@ -73,5 +73,14 @@ $(document).ready(function() {
 
     $("[data-fancybox]").fancybox();
 
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
 
 });
