da621b30319d1435875363fac96bfd038c91700a
Efosa Osazuwa <efosazuwa0@gmail.com>
2019-05-08 15:30:00 -0600

    created a sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -101,6 +101,23 @@ $(document).ready(function() {
         return false;
     });
 
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickNavigation);
+
+    function stickNavigation() {
+
+        const body = $("body");
+
+        if($(window).scrollTop() >= navTop) {
+            body.addClass("fixedNav");
+        }
+        else {
+            body.removeClass("fixedNav");
+        }
+    }
+
 
 
 });
